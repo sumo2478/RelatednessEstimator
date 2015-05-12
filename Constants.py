893be898ede2cc9e7c707d44c786c5336a8ab2a0
Collin Yen <collinyen7@gmail.com>
2015-05-12 10:28:06 -0700

    Added Constants file and simplified data set

---
+++ Constants.py
@@ -0,0 +1,19 @@
+# Relationship mapping constants
+RELATIONSHIP_PARENT1_ID_INDEX = 2
+RELATIONSHIP_PARENT2_ID_INDEX = 3
+RELATIONSHIP_REGION_INDEX = 6
+
+# Genotype Data Constants
+ALLELE_TO_INDEX_MAP = {
+	'A': 0,
+	'C': 1,
+	'G': 0,
+	'T': 1
+}
+
+TRANSMITTED_MAP = {
+	'_A': True,
+	'_B': False
+}
+
+CHROMOSOME_PARENT_INDEX = 0
